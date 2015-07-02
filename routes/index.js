23a7e882d646a817d52759aaedc59ca1c2c3537b
= <vilorio@outlook.com>
2015-07-02 15:59:49 -0400

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,7 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', function(req, res){
+    res.render("author",  { });
+});
 module.exports = router;
