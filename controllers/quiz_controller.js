b24f688a25752b54f8f5d56fa4894a0cc43e06c3
= <vilorio@outlook.com>
2015-07-28 15:44:10 -0400

    Test 072805

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,45 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si ruta incluye ;quizId
+exports.load = function (req, res, next, quizId) {
+    models.Quiz.find(quizId).then(
+        function (quiz) {
+            if (quiz) {
+                req.quiz = quiz;
+                next();
+            } else { next(new Error('No existe quizId=' + quizId)); } 
+        }
+    ).catch(function(error) { next(error);});
+};
 // Get /quizes
 exports.index = function (req, res) {
-    models.Quiz.findAll().then(function (quizes) {
+    models.Quiz.findAll().then(
+    function (quizes) {
         res.render('quizes/index.ejs', { quizes: quizes});
-    });
+    }).catch(function(error) { next(error);});
 };
 
 //Get/quizes/:id
 exports.show = function (req, res) {
-    models.Quiz.find(req.params.quizId).then(function (quiz) { 
-        res.render("quizes/show", { quiz: quiz});
-    });    
+    res.render('quizes/show', { quiz:req.quiz});
+    //models.Quiz.find(req.params.quizId).then(function (quiz) { 
+    //    res.render("quizes/show", { quiz: quiz});
+    //});    
 };
 
 //Get/quizes/:id/answer  
 exports.answer = function (req, res) {
-    models.Quiz.find(req.params.quizId).then(function (quiz) { 
-    if (req.query.respuesta === quiz.respuesta) {
-        res.render("quizes/answer",  { quiz: quiz, respuesta: "Correcto" });
+    var result = 'Incorrecto';
+    if(req.query.respuesta === req.quiz.respuesta){
+        resultado = 'Correcto';
     }
-    else {
-        res.render("quizes/answer", { quiz: quiz,  respuesta: "incorrecto" });
-    }        
-    }); 
+    res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+    //models.Quiz.find(req.params.quizId).then(function (quiz) { 
+    //if (req.query.respuesta === quiz.respuesta) {
+    //    res.render("quizes/answer",  { quiz: quiz, respuesta: "Correcto" });
+    //}
+    //else {
+    //    res.render("quizes/answer", { quiz: quiz,  respuesta: "incorrecto" });
+    //}        
+    //}); 
 };
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
