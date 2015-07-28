af61f1bae2d737acc0e99bd3e8eeb92bba2a563e
= <vilorio@outlook.com>
2015-07-28 11:33:42 -0400

    Test 072806

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ router.get('/', function(req, res) {
 });
 
 router.get('/quizes',                       quizController.index);
-router.get('/quizes/:quizid(\\d+)',         quizController.show);
-router.get('/quizes/:quizid(\\d+)/answer',  quizController.answer);
+router.get('/quizes/:quizId(\\d+)',         quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 module.exports = router;
