ebd99228237494bbb67514881aa1049a126e7be8
= <vilorio@outlook.com>
2015-08-17 13:44:25 -0400

    Crear preguntas 4

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 router.get('/quizes/new',                   quizController.new);
-router.get('/quizes/create',                quizController.create);
+router.post('/quizes/create',               quizController.create);
 //router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
 //router.get('/quizes/:quizId(\\d+)',         quizController.update);
 router.get('/author',                       quizController.author);
